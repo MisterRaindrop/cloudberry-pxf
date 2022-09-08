330e440a6ca465328f347f32ca357528a5dbb2d2
Himanshu Pandey <pandeyhi@vmware.com>
2022-09-08 13:49:01 -0700

    Changes for compiling PXF with GPDB 6.x

--- external-table/src/pxfuriparser.c
+++ external-table/src/pxfuriparser.c
@@ -290,7 +290,7 @@ GPHDUri_verify_no_duplicate_options(GPHDUri *uri)
 	foreach(option, uri->options)
 	{
 		OptionData *data = (OptionData *) lfirst(option);
-
+#if PG_VERSION_NUM >= 120000
 		Value	   *key = makeString(asc_toupper(data->key, strlen(data->key)));
 
 		if (!list_member(previousKeys, key))
